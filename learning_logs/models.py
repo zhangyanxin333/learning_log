efb712363fcb33bbbca2b26a575140e2194be77a
zhangyanxin333 <15980368907@163.com>
2017-11-20 21:50:51 +0800

    Started project.

---
+++ learning_logs/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+class Topic(models.Model):
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User)
+
+    def __str__(self):
+        return self.text
+class Entry(models.Model):
+    topic = models.ForeignKey(Topic)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.text[:50] +"..."
